6c1770341e48b80456194587f69f315ed6b89b39
JoJoEnRu <95616317+SichkarIvan@users.noreply.github.com>
2022-03-23 18:45:17 +0700

    Bug

--- src/Logic.hs
+++ src/Logic.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ParallelListComp #-}
 module Logic where
 
 import Data.List
@@ -17,15 +18,13 @@ checkWinPole field player i rows block = checkWinPlCustom (changeInField field p
 listOfWin :: [[Player]] -> Player -> Int -> Int -> [(Int, Int)]
 listOfWin field player rows block = [i | i <- listOfFree field rows, checkWinPole field player i rows block]
 
---countOfWins :: [[Player]] -> Player -> Int -> Int -> Int
---countOfWins field player rows block     | not $ null $ listOfWin field player rows block        = length $ listOfWin field player rows block
---                                        | not $ null $ listOfWin field (rPl player) rows block  = negate (length $ listOfWin field (rPl player) rows block)
---                                        | otherwise  = 0
 countOfWins :: [[Player]] -> Player -> Int -> Int -> (Int, Int) -> Int
 countOfWins field player rows block i   = length $ listOfWin (changeInField field player i) player rows block
 
 countOfLose :: [[Player]] -> Player -> Int -> Int -> (Int, Int) -> Int
 countOfLose field player rows block i   = length $ listOfWin (changeInField field (rPl player) i) (rPl player) rows block
+
+--
 
 callForBot :: [[Player]] -> Player -> IO (Int, Int)
 callForBot field player = do
@@ -35,15 +34,13 @@ callForBot field player = do
     let lose1 = listOfWin field (rPl player) rows block
     let goodPole = sortBy (\a b -> if countOfWins field player rows block a > countOfWins field player rows block b then LT else GT) (listOfFree field rows)
     let badPole = sortBy (\a b -> if countOfLose field player rows block a > countOfLose field player rows block b then LT else GT) (listOfFree field rows)
-    --let test = [(i, countOfWins (changeInField field player i) player rows block) | i <- goodPole]
-    --when debugging $ print test
     lastChanse <- callForBotRandom field player
     if not (null win1)
         then return $ head win1
         else if not (null lose1)
                 then return $ head lose1
                 else if countOfWins field player rows block (head goodPole) > 1
-                    then return $ head goodPole-- lastChanse
+                    then return $ head goodPole
                     else if countOfLose field player rows block (head badPole) > 1
                         then return $ head badPole
                         else return lastChanse
@@ -63,4 +60,11 @@ callForBotRandom field player = do
 
 
 callForHardBotRandom :: [HardPlayers] -> Player -> IO (FieldNumber, Int, Int)
-callForHardBotRandom field player = undefined
+callForHardBotRandom field player = do
+    let pole = [fieldP i | i <- field, isActiveFieldP i]
+    let poleId = [i | i <- [0..8] | j <- field, isActiveFieldP j]
+    s <- callForBot (head pole) player
+    g <- newStdGen
+    if length pole == 1
+        then return (head poleId, fst s, snd s)
+        else return (0,0,0)
